8a66604a25962943a20155e363ad8e9999acabdf
Wellington Defassio <well.defassio@gmail.com>
2022-08-27 18:14:37 -0300

    commit: mudi project - alura

---
+++ src/main/java/br/com/loja/mudi/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package br.com.loja.mudi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests().anyRequest().authenticated().and().formLogin(form -> form
+                .loginPage("/login")
+                .permitAll()
+        ).logout(logout -> logout.logoutUrl("/logout"));
+        return http.build();
+    }
+
+
+}
